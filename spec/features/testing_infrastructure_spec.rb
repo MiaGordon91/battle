a9b067823028c532edd88cfa7eef636b44bd6195
Jack Storror <jackstorror@gmail.com>
2021-10-27 16:43:15 +0100

    testing infrastructure

---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,9 @@
+require './app'
+
+feature 'Testing Infrastructure' do
+
+  scenario 'Page displays text - Testing infrastructure working!' do
+    visit '/'
+    expect(page).to have_content 'Testing infrastructure working!'  
+  end
+end
